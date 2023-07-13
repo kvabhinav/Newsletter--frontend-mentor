871fa75bd9adf3e6b25ea577300626c1ea0189c9
kvabhinav <abhinavkv888@gmail.com>
2023-07-13 23:21:34 +0530

    success page design

--- src/pages/Success.js
+++ src/pages/Success.js
@@ -1,7 +1,14 @@
-export default function Success(){
-    return(
-        <div>
-            <h1 className="">Success page</h1>
+import iconlist from '../images/iconlist.svg'
+
+export default function Success() {
+    return (
+        <div className="bg-white w-[400px] h-[420px] rounded-3xl p-10 flex flex-col justify-between items-center">
+            <div className="mt-1 lg:mt-0 w-12 h-12 bg-tomato rounded-full flex justify-center lg:items-center self-start">
+                <img src={iconlist} alt="tic symbol" width={50} height={50}></img>
+            </div>
+            <h1 className='text-5xl font-bold text-darkSlateGrey'>Thanks for subscribing!</h1>
+            <p className='text-sm font-medium text-darkSlateGrey'>A confirmation email has been sent to <span className='font-bold'>ash@gmail.com</span> Please open it and click the button inside to confirm your subscription.</p>
+            <button className='text-white font-medium bg-darkSlateGrey hover:bg-gradient-to-r from-pink-500 to-tomato   w-80  lg:w-full h-14 text-center rounded-lg'>Dismiss message</button>
         </div>
     )
 }
